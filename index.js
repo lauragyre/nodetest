d5ff501eced77dc0da12a76c30ba5bc7803d0fda
Laura Gyre <laura@trainingwingsworkshop.com>
2020-05-13 12:49:42 -0400

    this is it

---
+++ index.js
@@ -0,0 +1,25 @@
+let http = require('http');
+let url = require('url');
+let fs = require('fs');
+
+http.createServer(function(req, res){
+    const fileName = url.parse(req.url, true);
+    const file = "." + fileName.pathname;
+    fs.readFile(file, function(err, data) {
+        if (err) {
+                fs.readFile('./error.html', function(err, data) {
+                    if (err) {
+                        res.writeHead(404, {'Content-Type': 'text/html'});
+                        return res.end("404 Not Found");
+                    }
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.write(data);
+                    return res.end();
+                })
+            } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+            }
+    });
+}).listen(8080);
